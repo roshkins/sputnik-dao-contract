c12be3a98b39a5db6af98903c6bd7b2b04bb7c2a
Rashi Abramson <roshkins@gmail.com>
2021-09-08 11:50:34 -0700

    Add NFTCore trait for cross contract calls

--- sputnik-nft-staking/src/lib.rs
+++ sputnik-nft-staking/src/lib.rs
@@ -1,5 +1,5 @@
-use near_contract_standards::non_fungible_token::core::{NonFungibleTokenCore, NonFungibleTokenReceiver};
-use serde::{Deserialize, Serialize};
+use near_contract_standards::non_fungible_token::core::{NonFungibleTokenReceiver};
+use near_contract_standards::non_fungible_token::{TokenId};
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
@@ -15,7 +15,83 @@ mod storage_impl;
 mod user;
 
 #[ext_contract(ext_non_fungible_token)]
-trait NonFungibleTokenCore: NonFungibleTokenCore {}
+pub trait NonFungibleTokenCore {
+    /// Simple transfer. Transfer a given `token_id` from current owner to
+    /// `receiver_id`.
+    ///
+    /// Requirements
+    /// * Caller of the method must attach a deposit of 1 yoctoⓃ for security purposes
+    /// * Contract MUST panic if called by someone other than token owner or,
+    ///   if using Approval Management, one of the approved accounts
+    /// * `approval_id` is for use with Approval Management,
+    ///   see https://nomicon.io/Standards/NonFungibleToken/ApprovalManagement.html
+    /// * If using Approval Management, contract MUST nullify approved accounts on
+    ///   successful transfer.
+    /// * TODO: needed? Both accounts must be registered with the contract for transfer to
+    ///   succeed. See see https://nomicon.io/Standards/StorageManagement.html
+    ///
+    /// Arguments:
+    /// * `receiver_id`: the valid NEAR account receiving the token
+    /// * `token_id`: the token to transfer
+    /// * `approval_id`: expected approval ID. A number smaller than
+    ///    2^53, and therefore representable as JSON. See Approval Management
+    ///    standard for full explanation.
+    /// * `memo` (optional): for use cases that may benefit from indexing or
+    ///    providing information for a transfer
+    fn nft_transfer(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approval_id: Option<u64>,
+        memo: Option<String>,
+    );
+
+    /// Transfer token and call a method on a receiver contract. A successful
+    /// workflow will end in a success execution outcome to the callback on the NFT
+    /// contract at the method `nft_resolve_transfer`.
+    ///
+    /// You can think of this as being similar to attaching native NEAR tokens to a
+    /// function call. It allows you to attach any Non-Fungible Token in a call to a
+    /// receiver contract.
+    ///
+    /// Requirements:
+    /// * Caller of the method must attach a deposit of 1 yoctoⓃ for security
+    ///   purposes
+    /// * Contract MUST panic if called by someone other than token owner or,
+    ///   if using Approval Management, one of the approved accounts
+    /// * The receiving contract must implement `ft_on_transfer` according to the
+    ///   standard. If it does not, FT contract's `ft_resolve_transfer` MUST deal
+    ///   with the resulting failed cross-contract call and roll back the transfer.
+    /// * Contract MUST implement the behavior described in `ft_resolve_transfer`
+    /// * `approval_id` is for use with Approval Management extension, see
+    ///   that document for full explanation.
+    /// * If using Approval Management, contract MUST nullify approved accounts on
+    ///   successful transfer.
+    ///
+    /// Arguments:
+    /// * `receiver_id`: the valid NEAR account receiving the token.
+    /// * `token_id`: the token to send.
+    /// * `approval_id`: expected approval ID. A number smaller than
+    ///    2^53, and therefore representable as JSON. See Approval Management
+    ///    standard for full explanation.
+    /// * `memo` (optional): for use cases that may benefit from indexing or
+    ///    providing information for a transfer.
+    /// * `msg`: specifies information needed by the receiving contract in
+    ///    order to properly handle the transfer. Can indicate both a function to
+    ///    call and the parameters to pass to that function.
+    fn nft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approval_id: Option<u64>,
+        memo: Option<String>,
+        msg: String,
+    ) -> PromiseOrValue<bool>;
+
+    /// Returns the token with the given `token_id` or `null` if no such token.
+    fn nft_token(&self, token_id: TokenId) -> Option<Token>;
+}
+
 
 #[derive(BorshStorageKey, BorshSerialize)]
 enum StorageKeys {
@@ -71,7 +147,8 @@ impl Contract {
         owner_id: AccountId,
         token_ids: UnorderedSet<String>,
         unstake_period: U64,
-        token_vote_weights: LookupMap<String, U128>,
+        //TODO: Optimize storage, see: https://stackoverflow.com/questions/69096013/how-can-i-serialize-a-near-sdk-rs-lookupmap-that-uses-a-string-as-a-key-or-is-t
+        token_vote_weights: UnorderedMap<String, U128>,
     ) -> Self {
         Self {
             owner_id: owner_id.into(),
