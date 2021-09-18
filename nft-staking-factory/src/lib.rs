d3475868b11af43ec0a009f8f393a83f31510e49
Rashi Abramson <roshkins@gmail.com>
2021-09-17 18:01:18 -0700

    change method for standard and rebuild

---
+++ nft-staking-factory/src/lib.rs
@@ -0,0 +1,137 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::UnorderedSet;
+use near_sdk::json_types::{Base64VecU8, U128};
+use near_sdk::{assert_self, env, ext_contract, near_bindgen, AccountId, Gas, Promise, PublicKey};
+
+const CODE: &[u8] = include_bytes!("../../sputnik-nft-staking/res/sputnik_nft_staking.wasm");
+
+/// Gas spent on the call & account creation.
+const CREATE_CALL_GAS: Gas = Gas(75_000_000_000_000);
+
+/// Gas allocated on the callback.
+const ON_CREATE_CALL_GAS: Gas = Gas(10_000_000_000_000);
+
+#[ext_contract(ext_self)]
+pub trait ExtSelf {
+    fn on_create(
+        &mut self,
+        account_id: AccountId,
+        attached_deposit: U128,
+        predecessor_account_id: AccountId,
+    ) -> bool;
+}
+
+#[near_bindgen]
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct StakingContractFactory {
+    contracts: UnorderedSet<AccountId>,
+}
+
+impl Default for StakingContractFactory {
+    fn default() -> Self {
+        env::panic_str("NFT Staking Contract should be initialized before usage")
+    }
+}
+
+#[near_bindgen]
+impl StakingContractFactory {
+    #[init]
+    pub fn new() -> Self {
+        assert!(!env::state_exists(), "The contract is already initialized");
+        Self {
+            contracts: UnorderedSet::new(b"c".to_vec()),
+        }
+    }
+
+    pub fn get_contract_list(&self) -> Vec<AccountId> {
+        self.contracts.to_vec()
+    }
+
+    #[payable]
+    pub fn create(
+        &mut self,
+        name: AccountId,
+        public_key: Option<PublicKey>,
+        args: Base64VecU8,
+    ) -> Promise {
+        let account_id: AccountId = format!("{}.{}", name, env::current_account_id())
+            .parse()
+            .unwrap();
+        let mut promise = Promise::new(account_id.clone())
+            .create_account()
+            .deploy_contract(CODE.to_vec())
+            .transfer(env::attached_deposit());
+        if let Some(key) = public_key {
+            promise = promise.add_full_access_key(key.into())
+        }
+        promise
+            .function_call(
+                "new".to_string(),
+                args.into(),
+                0,
+                env::prepaid_gas() - CREATE_CALL_GAS - ON_CREATE_CALL_GAS,
+            )
+            .then(ext_self::on_create(
+                account_id,
+                U128(env::attached_deposit()),
+                env::predecessor_account_id(),
+                env::current_account_id(),
+                0,
+                ON_CREATE_CALL_GAS,
+            ))
+    }
+
+    pub fn on_create(
+        &mut self,
+        account_id: AccountId,
+        attached_deposit: U128,
+        predecessor_account_id: AccountId,
+    ) -> bool {
+        assert_self();
+        if near_sdk::is_promise_success() {
+            self.contracts.insert(&account_id);
+            true
+        } else {
+            Promise::new(predecessor_account_id).transfer(attached_deposit.0);
+            false
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use near_sdk::test_utils::{accounts, testing_env_with_promise_results, VMContextBuilder};
+    use near_sdk::{testing_env, PromiseResult};
+
+    use super::*;
+
+    #[test]
+    fn test_basics() {
+        let mut context = VMContextBuilder::new();
+        testing_env!(context.current_account_id(accounts(0)).build());
+        let mut factory = StakingContractFactory::new();
+        testing_env!(context.attached_deposit(10).build());
+        factory.create(
+            "test".parse().unwrap(),
+            Some(
+                "ed25519:6E8sCci9badyRkXb3JoRpBj5p8C6Tw41ELDZoiihKEtp"
+                    .parse()
+                    .unwrap(),
+            ),
+            "{}".as_bytes().to_vec().into(),
+        );
+        testing_env_with_promise_results(
+            context.predecessor_account_id(accounts(0)).build(),
+            PromiseResult::Successful(vec![]),
+        );
+        factory.on_create(
+            format!("test.{}", accounts(0)).parse().unwrap(),
+            U128(10),
+            accounts(0),
+        );
+        assert_eq!(
+            factory.get_contract_list(),
+            vec![format!("test.{}", accounts(0)).parse().unwrap()]
+        );
+    }
+}
--- sputnik-nft-staking/src/lib.rs
+++ sputnik-nft-staking/src/lib.rs
@@ -315,7 +315,7 @@ impl NonFungibleTokenReceiver for Contract {
     fn nft_on_transfer(
         &mut self,
         sender_id: AccountId,
-        _previous_owner_id: AccountId,
+        previous_owner_id: AccountId,
         token_id: near_contract_standards::non_fungible_token::TokenId,
         msg: String,
     ) -> PromiseOrValue<bool> {
